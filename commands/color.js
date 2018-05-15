f0d584177f51760359e01fc0bf73ccc67b0884d9
ValentinHacker <alternativexxxy@gmail.com>
2018-05-15 13:23:49 +0000

    Oprimisation
    By Cheeze

--- commands/color.js
+++ commands/color.js
@@ -1,4 +1,6 @@
-const parent = module.parent.exports;
+const parent = module.parent.exports,
+client = parent.client,
+bot = parent.bot;
 
 exports.com = new RegExp('^' + parent.bot.prefix + 'col(or)?( .+)?$', 'i');
 exports.name = 'Color';
@@ -7,11 +9,17 @@ exports.level = 'User';
 exports.category = 'Utility';
 exports.description = `Color info.`;
 
-exports.command = function(msg, comm) {
+exports.command = async function(msg, comm) {
 	const message = new parent.Discord.RichEmbed();
 	message.setColor((comm.last()[1] || 'RANDOM').toUpperCase())
 	.setTitle(message.color);
-	msg.reply((comm.last()[1] || 'RANDOM').toUpperCase(), {
+	try {
+		await msg.reply((comm.last()[1] || 'RANDOM').toUpperCase(), {
 			embed: message
-	});
+		});
+	} catch(err) {
+		await msg.author.send((comm.last()[1] || 'RANDOM').toUpperCase(), {
+			embed: message
+		});
+	}
 };
