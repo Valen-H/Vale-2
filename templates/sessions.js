84fc3b54cc4ded51412ea13fdf8cf8b1e31bc21e
ValentinHacker <alternativexxxy@gmail.com>
2018-06-25 21:16:39 +0300

    Add files via upload

--- templates/sessions.js
+++ templates/sessions.js
@@ -1,1 +1,11 @@
+const parent = module.parent.exports;
 
+async function command(args) {
+	args.forEach(arg => {
+		parent.lin(`.session ${arg}`);
+		parent.lin(`.send ${arg}`);
+	});
+	return 0;
+} //command
+
+module.exports = new parent.Command('Sessions', 'Activate Spying and forwarding sessions on specific channel(s)', '(\\d+ )+', 'ids<Number[]>', command);
